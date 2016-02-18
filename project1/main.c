bb8d39da14d7ee545d572756526e8b4ab441e7ae
Nick Orf <norf315@gmail.com>
2016-02-17 20:39:07 -0600

    got the specific target to work

--- project1/main.c
+++ project1/main.c
@@ -6,11 +6,9 @@
 #include <sys/types.h>
 #include <sys/wait.h> 
 #include <unistd.h>
+#include <time.h>
 
 #include "util.h"
-
-#define CREATE_FLAGS (O_WRONLY | O_CREAT | O_APPEND)
-#define CREATE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 //This function will parse makefile input from user or default makeFile 
 int parse(char * lpszFileName, struct target targets[20]) {
 	int nLine=0;
@@ -112,9 +110,11 @@ int main(int argc, char **argv) {
 	int nflag = 0;
 	int mflag = 0;
 	int bflag = 0;
+	int targetflag = 0;
 	char * format = "f:hnBm:";
 	int fd;
 	int time;
+	char * specifictarget;
 	
 	
 	// Default makefile name will be Makefile
@@ -170,6 +170,8 @@ int main(int argc, char **argv) {
 		 * appropriately, and then build the target.
 	*/
 		 printf("---the specific target we are supposed to build is %s---\n\n",	 *argv);
+		 specifictarget = *argv;
+		 targetflag = 1;
 	}
 	else {
 		//set target to be first target from makefile
@@ -189,7 +191,7 @@ int main(int argc, char **argv) {
 	else {
 		int i;
 		int y;
-		/*for(i=0; i<numTargets; i+=1){
+		/*for(i=0; i<numTargets; i+=1){//prints out all targets
 			printf("target = %s\n", targets[i].szTarget);
 			printf("command = %s\n", targets[i].szCommand);
 			printf("dependency count = %d\n", targets[i].nDependencyCount);
@@ -197,13 +199,25 @@ int main(int argc, char **argv) {
 				printf("dependency = %s\n", targets[i].szDependencies[y]);
 			}
 		}*/
-		if(mflag){
-			fd = open(szLog, O_WRONLY);
-			if(fd == -1){
+		if(targetflag){//if there is a specific target to be built
+			for(x=0; x<numTargets; x+=1){//search through all the targets for the right one
+				if(strcmp(targets[x].szTarget, specifictarget)==0){//if the target matches the right target
+					break;//break, therefore x will be set to the specific target
+				}
+			}
+			if(x==numTargets){
+				printf("failed to find target: %s\n",specifictarget);
+				perror("failed to find target");
+				exit(3);
+			}
+		}
+		if(mflag){//if the m flag is set, then it should have already opened the file or created it
+			fd = open(szLog, O_WRONLY);//get the file descriptor
+			if(fd == -1){//if it failed to open quit
 				perror("Failed to open");
 				exit(0);
 			}
-			if(dup2(fd,STDOUT_FILENO)==-1){
+			if(dup2(fd,STDOUT_FILENO)==-1){//then change the standard out to our file using dup2
 				perror("Failed to redirect stdout.");
 				exit(0);
 			}
